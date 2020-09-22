532902b9865fa4bcc8f3e6a3172db22242c2eed0
justinefrange@gmail.com <Stu@Rtlittle123>
2020-09-22 21:12:37 +0800

    fix

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'pms_merge',
+	'hostname' => 'dssc.cae0awknguro.ap-northeast-1.rds.amazonaws.com',
+	'username' => 'pms',
+	'password' => 'RjKXAMwbDrITLCEG',
+	'database' => 'pms',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
